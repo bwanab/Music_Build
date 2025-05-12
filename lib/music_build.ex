20f0594e47e62034ceb9a33cab5a7c415efe2657
Bill Allen <bill@k2bea.org>
2025-05-11 21:24:48 -0400

    initial commit

---
+++ lib/music_build.ex
@@ -0,0 +1,18 @@
+defmodule MusicBuild do
+  @moduledoc """
+  Documentation for `MusicBuild`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MusicBuild.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
