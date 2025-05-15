2eb868961acf3d03d3e205e5567a065404a32a1b
Bill Allen <bill@k2bea.org>
2025-05-15 15:50:57 -0400

    move lily stuff here from music_prims

--- lib/examples/midi_from_scratch.ex
+++ lib/examples/midi_from_scratch.ex
@@ -89,7 +89,7 @@ defmodule MusicBuild.Examples.MidiFromScratch do
   def write_file(notes, name, out_type \\ :midi) do
     case out_type do
       :midi -> write_midi_file(notes, name)
-      :lily -> Lily.write(notes, "test/#{name}.ly", midi: true, title: name, out_path: "./test")
+      :lily -> MusicBuild.Examples.Lily.write(notes, "test/#{name}.ly", midi: true, title: name, out_path: "./test")
     end
   end
 
