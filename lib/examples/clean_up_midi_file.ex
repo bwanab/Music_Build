eb303ef4d1930a03c42cc921e413d61fc3023b1c
Bill Allen <bill@k2bea.org>
2025-05-26 12:11:55 -0400

    add a module of midi clean up utilities

---
+++ lib/examples/clean_up_midi_file.ex
@@ -0,0 +1,44 @@
+defmodule MusicBuild.Examples.CleanUpMidiFile do
+  alias MusicBuild.TrackBuilder
+  alias Midifile.Sequence
+
+  @doc """
+  removes all notes that have a duration of less than a 16th note
+
+  Note that this currently assumes a file with one track!
+  """
+  def filter_bad_notes(pathname) do
+    seq = Midifile.Reader.read(pathname)
+    filtered_seq = Filter.process_notes(seq, 0, fn note -> note.duration < 0.1 end, :remove)
+    outpath = Path.join(Path.dirname(pathname), "filtered_" <> Path.basename(pathname))
+    IO.inspect(outpath)
+    Midifile.Writer.write(filtered_seq, outpath)
+  end
+
+  @doc """
+  bumps a midi files notes up one octave.
+
+  NOTE: this only works for tracks that contain no Arpeggios or Chords at present!
+  """
+  def bump_octave(pathname) do
+    seq = Midifile.Reader.read(pathname)
+    track = Enum.at(seq.tracks, 0)
+    sonorities = MapEvents.track_to_sonorities(track, ticks_per_quarter_note: seq.ticks_per_quarter_note)
+    new_sonorities = Enum.map(sonorities, fn s ->
+      case Sonority.type(s) do
+        :note -> Note.bump_octave(s, :up)
+      _ -> s
+      end
+    end)
+    outpath = Path.join(Path.dirname(pathname), "bumped_octave_" <> Path.basename(pathname))
+    write_midi_file([new_sonorities], outpath)
+  end
+
+  def write_midi_file(notes, outpath) do
+    name = Path.basename(outpath, Path.extname(outpath))
+    tracks = Enum.map(notes, fn track -> TrackBuilder.new(name, track, 960) end)
+    sfs = Sequence.new(name, 110, tracks, 960)
+    Midifile.write(sfs, outpath)
+  end
+
+end
